0ca4aed1bfb705f8da0ee5572b2af4ceea488517
usrtax <i@user.tax>
2023-03-10 14:55:01 +0800

    ♨

--- ru/src/lib.rs
+++ ru/src/lib.rs
@@ -1,7 +1,9 @@
-const y = new Proxy([], {
-  get(_, key) {
-    console.log(typeof(y))
-    return _[key];
-  }
-});
-        
+use neon::prelude::*;
+
+#[neon::main]
+fn main(mut cx: ModuleContext) -> NeonResult<()> {
+  misc::init(&mut cx)?;
+  redis::init(&mut cx)?;
+  svg2webp::init(&mut cx)?;
+  Ok(())
+}
